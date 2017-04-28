ac7c829a61ef363bb964d22542c9c8021e362b6f
MaximKiselyow <m.kiselyow@gmail.com>
2017-04-28 07:59:39 +0300

    hw

--- lessons/flow-4/lesson-3/proga.rb
+++ lessons/flow-4/lesson-3/proga.rb
@@ -7,29 +7,42 @@
 # Ограничения: программа должна получать ссылку от меня в интерактивном режиме (используя метод gets)
 
 # Программа:
+require 'nokogiri'
+require 'open-uri'
+# require 'expect'
+# require 'rspec'
+# require 'spec_helper'
+
 class Proga
 
   # 1. получает ссылку на страницу
-  def add_link(link)
-    #code
+  def add_link(link = 'http://www.nokogiri.org/tutorials/installing_nokogiri.html')
+    @link = link
   end
 
   # 2. выбирает оттуда все ссылки которые найдет
   def extract_links
-    #code
+    @print_links = []
+    doc = Nokogiri::HTML.parse(open(@link))
+    puts "### Search for href's by css"
+    doc.css('a').map do |link|
+      @print_links << link['href'] if link['href'].include? "http"
+    end
   end
 
   # 3. и выводит их на экран
   def print_links
-    #code
+    puts @print_links
   end
 
 end
 
-class ProgaTest
-  def should_get_link
+# class ProgaTest
+#   def should_get_link
     proga = Proga.new
-    proga.add_link('http://www.yandex.ru')
-    expect(proga.links).to_include 'http://www.yandex.ru'
-  end
-end
+    proga.add_link('https://www.yandex.ru')
+    proga.extract_links()
+    # expect(proga.print_links).to_include 'https://www.yandex.ru'
+    proga.print_links()
+#   end
+# end
